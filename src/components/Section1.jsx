2ff399fc6a339e16becedbe5b1b3eee11539206f
jhigger <njohnjhigger@gmail.com>
2021-12-11 21:14:38 +0800

    edit

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -11,26 +11,17 @@ import pizzastack from '../assets/pizzastack.webp';
 
 export default function Section1() {
 	return (
-		<Box bg="black" color="white" w="full" h="2xl" position="relative" pt={24}>
-			<Container
-				maxW="container.xl"
-				h="full"
-				justify="center"
-				align="center"
-				position="relative"
-				overflow={{base: 'hidden', md: 'visible'}}
-			>
-				<SimpleGrid
-					mr="4"
-					columns={{base: 1, md: 2}}
-					spacing="1"
-					h="110%"
-					position="absolute"
-					top={{base: '0', md: '-10vh'}}
-				>
-					<Text m="10" alignSelf="center" fontSize={'xl'} fontWeight="thin">
+		<Box bg="black" color="white" w="full" h="min" position="relative">
+			<Container maxW="container.xl" justify="center" align="center" p="12">
+				<SimpleGrid columns={{base: 1, lg: 2}} spacing="1">
+					<Text
+						m="10"
+						alignSelf="center"
+						fontSize={{base: 'lg', lg: 'xl'}}
+						fontWeight="thin"
+					>
 						<Heading
-							fontSize="3xl"
+							fontSize={{base: 'xl', lg: '3xl'}}
 							fontWeight="bold"
 							letterSpacing="widest"
 							mb="12"
