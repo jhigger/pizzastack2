82773544ec94d7193fd3260028eb268f7f1f81ec
jhigger <njohnjhigger@gmail.com>
2021-12-12 07:07:21 +0800

    added faqs

--- src/pages/Franchise/Franchise.jsx
+++ src/pages/Franchise/Franchise.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import Form from './Form/Form';
-import {Box} from '@chakra-ui/react';
+import {Box, Container, Image} from '@chakra-ui/react';
+import FAQs from './FAQs';
+import why from '../../assets/why.webp';
 
 export default function Franchise() {
 	return (
 		<Box pt={24}>
+			<Container maxW="container.xl" justify="center" align="center" p="12">
+				<Image
+					loading="eager"
+					objectFit="cover"
+					alignSelf="center"
+					justifySelf="center"
+					src={why}
+					alt="why"
+				/>
+			</Container>
+			<FAQs />
 			<Form />
 		</Box>
 	);
