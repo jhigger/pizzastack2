cb7cd3eeb6c240ff251f133ff41a17f47c27144a
jhigger <njohnjhigger@gmail.com>
2021-12-11 13:13:25 +0800

    fixed datetime picker

--- src/pages/Franchise/Form/DateTimePicker.jsx
+++ src/pages/Franchise/Form/DateTimePicker.jsx
@@ -1,44 +1,23 @@
-import React, {useState} from 'react';
-import DateFnsUtils from '@date-io/date-fns';
-import {DateTimePicker, MuiPickersUtilsProvider} from '@material-ui/pickers';
-import {useField, Field, useFormikContext} from 'formik';
+import {useField, FastField} from 'formik';
 import TextField from '@material-ui/core/TextField';
 
 export default function BasicDateTimePicker({name, ...props}) {
 	const [field, meta] = useField(name);
-	const {setFieldValue} = useFormikContext();
-
-	const DateAndTimePicker = () => {
-		const [selectedDate, handleDateChange] = useState(new Date());
-
-		return (
-			<MuiPickersUtilsProvider utils={DateFnsUtils}>
-				<DateTimePicker
-					inputVariant="outlined"
-					value={selectedDate}
-					disablePast
-					onChange={() => {
-						handleDateChange();
-						setFieldValue(name, selectedDate);
-					}}
-					label="Date & Time"
-					showTodayButton
-					fullWidth
-				/>
-			</MuiPickersUtilsProvider>
-		);
-	};
 
 	return (
-		<Field
-			as={DateAndTimePicker}
+		<FastField
+			as={TextField}
 			fullWidth
 			variant="outlined"
+			type="datetime-local"
+			InputLabelProps={{
+				shrink: true
+			}}
 			name={name}
 			{...field}
 			{...props}
 			error={Boolean(meta.touched && meta.error)}
 			helperText={meta.touched && meta.error}
-		></Field>
+		></FastField>
 	);
 }
