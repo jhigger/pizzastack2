119c782499ae47de94869409674d57eb83249ef6
jhigger <njohnjhigger@gmail.com>
2021-12-17 09:24:09 +0800

    added page transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import React from 'react';
-import {
-	ChakraProvider,
-	Box,
-	Icon,
-	Container,
-	Flex,
-	VStack,
-	Text,
-	HStack,
-	SimpleGrid,
-	Image,
-	extendTheme
-} from '@chakra-ui/react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React, {useRef} from 'react';
+import {Switch, Route, useLocation} from 'react-router-dom';
 import Navbar from './components/Header/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -21,31 +8,30 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Franchise from './pages/Franchise/Franchise';
 import NotFound from './pages/NotFound';
-
-const config = {
-	initialColorMode: 'dark',
-	useSystemColorMode: false
-};
-
-const theme = extendTheme({config});
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
+import './App.css';
 
 function App() {
+	let location = useLocation();
+
 	return (
-		<ChakraProvider theme={theme}>
-			<Router>
-				<Navbar />
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route exact path="/home" component={Home} />
-					<Route exact path="/menu" component={Menu} />
-					<Route exact path="/about" component={About} />
-					<Route exact path="/contact" component={Contact} />
-					<Route exact path="/franchise" component={Franchise} />
-					<Route component={NotFound} />
-				</Switch>
-				<Footer />
-			</Router>
-		</ChakraProvider>
+		<>
+			<Navbar />
+			<TransitionGroup>
+				<CSSTransition key={location.pathname} classNames="fade" timeout={500}>
+					<Switch location={location}>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/home" component={Home} />
+						<Route exact path="/menu" component={Menu} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/contact" component={Contact} />
+						<Route exact path="/franchise" component={Franchise} />
+						<Route path="*" component={NotFound} />
+					</Switch>
+				</CSSTransition>
+			</TransitionGroup>
+			<Footer />
+		</>
 	);
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {ChakraProvider, extendTheme} from '@chakra-ui/react';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+const config = {
+	initialColorMode: 'dark',
+	useSystemColorMode: false
+};
+
+const theme = extendTheme({config});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+	<ChakraProvider theme={theme}>
+		<Router>
+			<App />
+		</Router>
+	</ChakraProvider>,
+	document.getElementById('root')
+);
