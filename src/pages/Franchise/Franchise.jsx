76dea9e052dcb135b2f56db3501cba2005703ac6
jhigger <njohnjhigger@gmail.com>
2022-04-09 01:57:26 +0800

    update

--- src/pages/Franchise/Franchise.jsx
+++ src/pages/Franchise/Franchise.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import Form from './Form/Form';
-import {Box, Container, Image} from '@chakra-ui/react';
-import why from '../../assets/images/why.webp';
+import {Box, Container} from '@chakra-ui/react';
+import Features from './Features';
 
 export default function Franchise() {
 	return (
 		<Box py={{base: 12, md: 24}} bg="black">
-			<Container
-				maxW="container.xl"
-				justify="center"
-				align="center"
-				p={{base: 1, md: 12}}
-			>
-				<Image
-					loading="eager"
-					objectFit="cover"
-					alignSelf="center"
-					justifySelf="center"
-					src={why}
-					alt="why"
-				/>
+			<Container maxW="container.xl" justify="center" align="center">
+				<Features />
 			</Container>
 			<Form />
 		</Box>
