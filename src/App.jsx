2dc0c4c3aa765007f54a37b43da050ac1d32e56f
jhigger <njohnjhigger@gmail.com>
2022-05-19 18:57:19 +0800

    added code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import {lazy, Suspense} from 'react';
 import {Switch, Route, useLocation} from 'react-router-dom';
 import Navbar from './components/Header/Navbar';
 import Footer from './components/Footer';
 import Home from './routes/Home';
-import Menu from './routes/Menu';
-import About from './routes/About';
-import Contact from './routes/Contact';
-import Franchise from './routes/Franchise';
-import NotFound from './routes/NotFound';
 import {AnimatePresence} from 'framer-motion';
+
+const Menu = lazy(() => import('./routes/Menu'));
+const About = lazy(() => import('./routes/About'));
+const Contact = lazy(() => import('./routes/Contact'));
+const Franchise = lazy(() => import('./routes/Franchise'));
+const NotFound = lazy(() => import('./routes/NotFound'));
 
 function App() {
 	let location = useLocation();
@@ -15,18 +17,20 @@ function App() {
 	return (
 		<>
 			<Navbar path={location.pathname} />
-			<AnimatePresence exitBeforeEnter initial={false}>
-				<Switch location={location} key={location.pathname}>
-					<Route exact path="/" component={Home} />
-					<Route exact path="/home" component={Home} />
-					<Route exact path="/menu" component={Menu} />
-					<Route exact path="/about" component={About} />
-					<Route exact path="/contact" component={Contact} />
-					<Route exact path="/franchise" component={Franchise} />
-					<Route path="*" component={NotFound} />
-				</Switch>
-			</AnimatePresence>
-			<Footer />
+			<Suspense fallback={<></>}>
+				<AnimatePresence exitBeforeEnter initial={false}>
+					<Switch location={location} key={location.pathname}>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/home" component={Home} />
+						<Route exact path="/menu" component={Menu} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/contact" component={Contact} />
+						<Route exact path="/franchise" component={Franchise} />
+						<Route path="*" component={NotFound} />
+					</Switch>
+				</AnimatePresence>
+				<Footer />
+			</Suspense>
 		</>
 	);
 }
