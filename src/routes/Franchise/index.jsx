cf1e862e401c50db3b3d8fea4a58305114472247
jhigger <njohnjhigger@gmail.com>
2022-05-20 13:52:30 +0800

    preload image

--- src/routes/Franchise/index.jsx
+++ src/routes/Franchise/index.jsx
@@ -1,11 +1,17 @@
-import {lazy, Suspense} from 'react';
-import {Box, Container, Image} from '@chakra-ui/react';
+import {lazy, Suspense, useEffect} from 'react';
+import {Box, Container, Image as Img} from '@chakra-ui/react';
 import PageTransition from '../../components/PageTransition';
 import why from '../../assets/images/why.webp';
 
 const Form = lazy(() => import('./Form'));
 
 export default function Franchise() {
+	// Preload Img
+	useEffect(() => {
+		const img = new Image();
+		img.src = why;
+	}, []);
+
 	return (
 		<PageTransition>
 			<Box py={{base: 12, md: 24}} bg="black">
@@ -16,7 +22,7 @@ export default function Franchise() {
 					p={{base: 1, md: 12}}
 					mb={8}
 				>
-					<Image
+					<Img
 						loading="eager"
 						objectFit="cover"
 						src={why}
