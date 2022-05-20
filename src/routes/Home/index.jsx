3ca80112b333da3100704d34bd021d49f5972c68
jhigger <njohnjhigger@gmail.com>
2022-05-20 10:50:20 +0800

    rename

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,4 +1,4 @@
-import Header from './Hero';
+import Hero from './Hero';
 import Section1 from './Section1';
 import Section2 from './Section2';
 import PageTransition from '../../components/PageTransition';
@@ -6,7 +6,7 @@ import PageTransition from '../../components/PageTransition';
 export default function Home() {
 	return (
 		<PageTransition>
-			<Header />
+			<Hero />
 			<Section1 />
 			<Section2 />
 		</PageTransition>
