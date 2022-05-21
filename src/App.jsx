7181e18f235d44f520b4a1a1228cdebba3148d68
jhigger <njohnjhigger@gmail.com>
2022-05-21 17:37:43 +0800

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,7 @@ function App() {
 						<Route exact path="/about" component={About} />
 						<Route exact path="/contact" component={Contact} />
 						<Route exact path="/franchise" component={Franchise} />
-						<Route exact path="/404" component={NotFound} />
-						<Redirect to="/404" />
+						<Route component={NotFound} />
 					</Switch>
 				</AnimatePresence>
 				<Footer />
