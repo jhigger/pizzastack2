b5b49db4f72bce9f4612590d3e3ea8a2de617e76
jhigger <njohnjhigger@gmail.com>
2022-05-22 09:53:50 +0800

    revert

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-<<<<<<< HEAD
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()]
-=======
-import viteCompression from 'vite-plugin-compression';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		react(),
-		viteCompression({
-			verbose: true,
-			deleteOriginFile: true,
-			algorithm: 'brotliCompress'
-		})
-	]
->>>>>>> 6d6d054f52b056262895f7adc2722a39c2ecceb5
 });
